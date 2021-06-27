7a7f57efc1c762dd16186b95f27efb5fe61ae38a
superCB <1956699404@qq.com>
2021-06-27 11:17:50 +0800

    i know the front how to communicate with back

--- Front/vue2/learn/src/components/Test.vue
+++ Front/vue2/learn/src/components/Test.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+  <div>
+
+    <el-button type="primary" plain @click="postLoginInfor"  >主要按钮</el-button>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Test'
+  name: 'Test',
+  data () {
+    return {
+      msg: 'dfasd'
+    }
+  },
+  methods: {
+    postLoginInfor () {
+      this.axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5000/login',
+        data: {
+          name: '张辉',
+          sex: '男'
+        }
+      })
+    }
+  }
+
 }
 </script>
 
--- Front/vue2/learn/src/main.js
+++ Front/vue2/learn/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
